4a341e78d93ab4fbd74d6e27e46320800b435a08
Ville Vaarala <ville.l.vaarala@gmail.com>
2019-09-23 21:47:00 +0300

    Use postgres with docker-compose

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ WORKDIR /app
 
 EXPOSE 8000
 
-CMD python manage.py runserver 0.0.0.0:8000
+CMD python manage.py syncdb --noinput && python manage.py runserver 0.0.0.0:8000
--- web/settings.py
+++ web/settings.py
@@ -28,12 +28,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'django.db.backends.sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = hakemisto + '/tupa.db'     # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'django.db.backends.' + os.getenv('DB_TYPE', 'sqlite3')           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = os.getenv('DB_NAME', hakemisto + '/tupa.db')     # Or path to database file if using sqlite3.
+DATABASE_USER = os.getenv('DB_USER', '')             # Not used with sqlite3.
+DATABASE_PASSWORD = os.getenv('DB_PASSWD', '')         # Not used with sqlite3.
+DATABASE_HOST = os.getenv('DB_HOST', '')             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = os.getenv('DB_PORT', '')             # Set to empty string for default. Not used with sqlite3.
+
 
 # Cache
 TAUSTALASKENTA = False # Tulokset lasketaan taustalla (Vaatii toimiakseen tomivan cachekokoonpanon)
